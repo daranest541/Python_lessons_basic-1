b384adcb1731a405f3af299344acf3ebd75eb99b
daranest541 <rjkmzy@gmail.com>
2018-09-18 19:59:21 +0300

    Домашнее задание к 4 уроку

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -1,14 +1,41 @@
+import random
 # Все задачи текущего блока решите с помощью генераторов списков!
 
 # Задание-1:
-# Дан список, заполненный произвольными целыми числами. 
+# Дан список, заполненный произвольными целыми числами.
 # Получить новый список, элементы которого будут
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
+random_list = random.sample(range(1, 20), 10)
+print(random_list)
+l2 = [i ** 2 for i in random_list]
+print(l2)
 
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+def fruit_to_list(fruit_str):
+    try:
+        if ',' in fruit_str:
+            return [f.strip() for f in fruit_str.split(',')]
+        else:
+            raise ValueError
+    except ValueError:
+        print('некоректный ввод данных')
+        exit()
+
+
+fruit1_str = input('Введите первый список фруктов через запятую: ')
+#fruit1_str = 'apple, banana, qiwi, orange'
+l1 = fruit_to_list(fruit1_str)
+fruit2_str = input('Введите второй список фруктов через запятую: ')
+#fruit1_str = 'banana, orange, pear'
+l2 = fruit_to_list(fruit1_str)
+
+res_list = [f for f in l1 if f in l2]
+print(res_list)
+
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -16,3 +43,8 @@
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+random_list2 = random.sample(range(-100, 100), 10)
+print(random_list2)
+new_list = [i for i in random_list2 if (i % 3 == 0) and i > 0 and i % 4 != 0]
+print(new_list)
--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,3 +1,5 @@
+import re
+import random
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
@@ -20,11 +22,31 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+#re
+list1 = re.findall(r'[a-z]+', line)
+print(list1)
+#without re
+st = ""
+lest2 = []
+i = 0
+for c in line:
+    if c.islower():
+        st += c
+    if c.isupper() and st != "":
+        lest2.append(st)
+        st = ""
+    i += 1
+lest2.append(str)
+
+print(lest2)
+
+
+
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
-# Т.е. из строки 
+# Т.е. из строки
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
@@ -45,9 +67,55 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+result_list = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print(result_list)
+
+result_list2 = []
+rem_i = 0
+for idx, ch in enumerate(line_2):
+     if ch.isupper() and line_2[idx - 1].islower() and line_2[idx - 2].islower():
+              i = idx
+              st = ''
+              while line_2[i].isupper():
+                     st += line_2[i]
+                     if i + 1 <= len(line_2) - 1:
+                           i += 1
+                     else:
+                            break
+              if len(st[:-2]) > 0:
+                     result_list2.append(st[:-2])
+print(result_list2)
+
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+
+rang = 2500
+file = open("test2500.txt", "w")
+for i in range(rang):
+       num = random.randint(0, 9)
+       file.write(str(num))
+
+file.close()
+
+with open('test2500.txt') as file:
+       num = file.read()
+rem = ''
+max_len = ''
+sq_list = []
+for i in num:
+       if i == rem:
+              max_len += i
+       else:
+              if len(max_len) > 1:
+                     sq_list.append(max_len)
+              max_len = ""
+
+       rem = i
+print(max(sq_list, key=len))
